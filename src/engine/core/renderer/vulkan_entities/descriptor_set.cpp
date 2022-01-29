baef76d079ff2c05d7dc14b48d794a4ab78e07dc
DESKTOP-TL9FLJG\Paolo Parker <parker9694@gmail.com>
2022-01-29 11:55:11 +0000

    Updated include

--- src/engine/core/renderer/vulkan_entities/descriptor_set.cpp
+++ src/engine/core/renderer/vulkan_entities/descriptor_set.cpp
@@ -4,6 +4,8 @@
 #include "logical_device.h"
 #include "descriptor_set.h"
 
+#include "descriptor_set.h"
+
 namespace Engine::Core::Renderer::VulkanEntities {
 	void DescriptorSet::CreateDescriptorSet(LogicalDevice& logical_device) {
 
