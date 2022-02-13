b94007a2f10aff701171066a4a462d49e3477f2b
DESKTOP-TL9FLJG\Paolo Parker <parker9694@gmail.com>
2022-02-13 10:13:05 +0000

    Setup checks for A, S and D keys in input

--- src/engine/input.cpp
+++ src/engine/input.cpp
@@ -18,6 +18,48 @@ void Input::key_callback(GLFWwindow* window, int key, int scancode, int action, 
 			Instance().isWHeldDown = false;
 		}
 	}
+
+	// A
+	if (key == GLFW_KEY_A) {
+		if (action == GLFW_PRESS) {
+			Instance().wasAPressed = true;
+			Instance().isAHeldDown = true;
+		}
+		else if (action == GLFW_REPEAT) {
+			Instance().isAHeldDown = true;
+		}
+		else {
+			Instance().isAHeldDown = false;
+		}
+	}
+
+	// S
+	if (key == GLFW_KEY_S) {
+		if (action == GLFW_PRESS) {
+			Instance().wasSPressed = true;
+			Instance().isSHeldDown = true;
+		}
+		else if (action == GLFW_REPEAT) {
+			Instance().isSHeldDown = true;
+		}
+		else {
+			Instance().isSHeldDown = false;
+		}
+	}
+
+	// D
+	if (key == GLFW_KEY_D) {
+		if (action == GLFW_PRESS) {
+			Instance().wasDPressed = true;
+			Instance().isDHeldDown = true;
+		}
+		else if (action == GLFW_REPEAT) {
+			Instance().isDHeldDown = true;
+		}
+		else {
+			Instance().isDHeldDown = false;
+		}
+	}
 }
 
 void Input::Init(GLFWwindow* window) {
@@ -25,16 +67,57 @@ void Input::Init(GLFWwindow* window) {
 }
 
 bool Input::IsKeyHeldDown(std::string key) {
-	if (key == "w") {
+	// W
+	if (key == "w" || key == "w") {
 		return Instance().isWHeldDown;
+	}
+
+	// A
+	if (key == "a" || key == "A") {
+		return Instance().isAHeldDown;
+	}
+
+	// S
+	if (key == "s" || key == "S") {
+		return Instance().isSHeldDown;
+	}
+
+	// D
+	if (key == "d" || key == "D") {
+		return Instance().isDHeldDown;
 	}
 	return false;
 }
 
 bool Input::WasKeyPressed(std::string key) {
-	if (key == "w") {
+	// W
+	if (key == "w" || key == "W") {
 		if (Instance().wasWPressed) {
 			Instance().wasWPressed = false;
+			return true;
+		}
+	}
+
+	// A
+	if (key == "a" || key == "A") {
+		if (Instance().wasAPressed) {
+			Instance().wasAPressed = false;
+			return true;
+		}
+	}
+
+	// S
+	if (key == "s" || key == "S") {
+		if (Instance().wasSPressed) {
+			Instance().wasSPressed = false;
+			return true;
+		}
+	}
+
+	// D
+	if (key == "d" || key == "D") {
+		if (Instance().wasDPressed) {
+			Instance().wasDPressed = false;
 			return true;
 		}
 	}
--- src/engine/input.h
+++ src/engine/input.h
@@ -1,9 +1,22 @@
 #pragma once
 
 class Input : public Singleton<Input> {
-	// Keys
+	// W
 	bool isWHeldDown;
 	bool wasWPressed;
+
+	// A
+	bool isAHeldDown;
+	bool wasAPressed;
+
+	// S
+	bool isSHeldDown;
+	bool wasSPressed;
+
+	// D
+	bool isDHeldDown;
+	bool wasDPressed;
+
 
 public:
 	static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
