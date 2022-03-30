daf2af2cd1d765b761e4f3aa38b48b4ba996ef4d
BISIM\paolo.parker <paolo.parker@bisimulations.com>
2022-03-30 09:40:30 +0100

    Added basic gltf parsing. Next step is to work on the JSON payload parsing

--- src/engine/GltfLoader.cpp
+++ src/engine/GltfLoader.cpp
@@ -1,15 +1,68 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 #include "GltfLoader.hpp"
+#include "Json.h"
+
+
+//uint32_t GltfLoader::GetIntValue(const char* string)
+//{
+//	for(size_t i=0; )
+//	return uint32_t();
+//}
+
+/// <summary>
+/// Converts an array of characters to an unsigned 32-bit integer
+/// </summary>
+/// <returns>The converted number</returns>
+uint32_t ToUInt32(const char* charArray) {
+	uint32_t result = 0;
+	for (int i = 0; i < sizeof(uint32_t); ++i) {
+		result |= (int)charArray[i] << (i * 8);
+	}
+	return result;
+}
 
 GltfData GltfLoader::Load(std::filesystem::path filename)
 {
-	GltfData data;
+	//GltfData data = ;
 	std::fstream file(filename, std::ios::binary | std::ios::in);
 	if (file.is_open()) {
-		std::cout << "File was opened" << std::endl;
+		std::cout << "Reading file " << filename << "\n";
+		GltfData data{};
+
+		// Header
+		unsigned int intSize = sizeof(uint32_t);
+		char* intBuffer = new char[intSize];
+		file.read(intBuffer, intSize);
+		data.header.magic = ToUInt32(intBuffer);
+		file.read(intBuffer, intSize);
+		data.header.version = ToUInt32(intBuffer);
+		file.read(intBuffer, intSize);
+		data.header.fileLength = ToUInt32(intBuffer);
+
+		// JSON
+		file.read(intBuffer, intSize);
+		data.json.chunkLength = ToUInt32(intBuffer);
+		file.read(intBuffer, intSize);
+		data.json.chunkType = ToUInt32(intBuffer);
+		data.json.data = new char[data.json.chunkLength];
+		file.read(data.json.data, data.json.chunkLength);
+		
+		// Binary
+		file.read(intBuffer, intSize);
+		data.binaryBuffer.chunkLength = ToUInt32(intBuffer);
+		file.read(intBuffer, intSize);
+		data.binaryBuffer.chunkType = ToUInt32(intBuffer);
+		data.binaryBuffer.data = new char[data.binaryBuffer.chunkLength];
+		file.read(data.binaryBuffer.data, data.binaryBuffer.chunkLength);
+
+		// Parse the JSON data
+		json::parsing::parse_results json = json::parsing::parse(data.json.data);
+		json::jobject obj = json::jobject::parse(json.value);
+		std::cout << std::endl;
 	}
-	return data;
+	return GltfData();
 }
--- src/engine/GltfLoader.hpp
+++ src/engine/GltfLoader.hpp
@@ -1,34 +1,43 @@
 #pragma once
 
 /// <summary>
-/// Represents binary gltf data
+/// Represents binary gltf data.
+/// Specification at https://www.khronos.org/registry/glTF/specs/2.0/glTF-2.0.html#glb-file-format-specification
 /// </summary>
 class GltfData {
 public:
 	// Header
 	struct {
-		uint32_t magic;
-		uint32_t version;
-		uint32_t fileLength;
+		uint32_t magic;			// Makes the file identifiable as a gltf file, it's a data format identifier
+		uint32_t version;		
+		uint32_t fileLength;	// File size in bytes
 	} header;
 	
 	// Chunk 0 (JSON)
 	struct {
 		uint32_t chunkLength;
-		uint32_t length;
+		uint32_t chunkType;
+		char* data;
 	} json;
 	
 	// Chunk 1 (Binary data)
 	struct {
 		uint32_t chunkLength;
-		uint32_t length;
-	} json;
+		uint32_t chunkType;
+		char* data;
+	} binaryBuffer;
 };
 
 /// <summary>
 /// Class used to load .glb or .gltf 3D files
 /// </summary>
 class GltfLoader {
+	/// <summary>
+	/// Sums the integer value of each character of string into one integer
+	/// </summary>
+	/// <param name="string">The string to convert to integer</param>
+	/// <returns>The sum of the values</returns>
+	//static uint32_t GetIntValue(const char* string);
 public:
 	static GltfData Load(std::filesystem::path filename);
 };
