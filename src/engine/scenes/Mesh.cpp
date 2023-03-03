5af11a03bbd8db111354d4917ec52b368e47beed
DESKTOP-UDDDLEQ\Paolo <parker9694@gmail.com>
2023-03-03 22:06:43 +0000

    Forgot to save a change.

--- src/engine/scenes/Mesh.cpp
+++ src/engine/scenes/Mesh.cpp
@@ -24,7 +24,26 @@ namespace Engine::Scenes
 {
     void Mesh::CreateShaderResources(Vulkan::PhysicalDevice& physicalDevice, VkDevice& logicalDevice)
     {
+		_data = Array<>
 
+		gameObject._mesh._shaderResources._objectDataBuffer = Buffer(_logicalDevice,
+			_physicalDevice,
+			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
+			&gameObject._transform._matrix,
+			sizeof(gameObject._transform._matrix));
+
+		gameObject._mesh._shaderResources._objectDataDescriptor = Descriptor(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 0, &gameObject._mesh._shaderResources._objectDataBuffer);
+		gameObject._mesh._shaderResources._textureDescriptor = Descriptor(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 0, nullptr, texture);
+
+		gameObject._mesh._shaderResources._objectDataSet = DescriptorSet(_logicalDevice, VK_SHADER_STAGE_VERTEX_BIT, { &gameObject._mesh._shaderResources._objectDataDescriptor });
+		gameObject._mesh._shaderResources._samplersSet = DescriptorSet(_logicalDevice, VK_SHADER_STAGE_FRAGMENT_BIT, { &gameObject._mesh._shaderResources._textureDescriptor });
+
+		gameObject._mesh._shaderResources._objectDataPool = DescriptorPool(_logicalDevice, { &gameObject._mesh._shaderResources._objectDataSet });
+		gameObject._mesh._shaderResources._samplersPool = DescriptorPool(_logicalDevice, { &gameObject._mesh._shaderResources._samplersSet });
+
+		gameObject._mesh._shaderResources._objectDataSet.SendDescriptorData();
+		gameObject._mesh._shaderResources._samplersSet.SendDescriptorData();
     }
 
     void Mesh::Update()
