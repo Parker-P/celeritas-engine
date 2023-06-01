cf86369a7d0bdc14a4fcea752b7a87b440b678eb
DESKTOP-OETM4MG\Paolo <parker9694@gmail.com>
2023-06-02 00:13:52 +0200

    Almost there with converting a spherical HDRi to a cube map.

--- src/engine/scenes/SphericalEnvironmentMap.cpp
+++ src/engine/scenes/SphericalEnvironmentMap.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 #include <GLFW/glfw3.h>
 #include <filesystem>
 #include <glm/gtc/matrix_transform.hpp>
 #include <stb/stb_image.h>
 #include <vulkan/vulkan.h>
+#include <stb/stb_image_write.h>
 
+#include "utils/Utils.hpp"
 #include "engine/vulkan/Queue.hpp"
 #include "engine/vulkan/PhysicalDevice.hpp"
 #include "engine/vulkan/Image.hpp"
@@ -48,7 +50,13 @@ namespace Engine::Scenes
 			LOWER
 		};
 
-		std::map<CubeMapFace, std::vector<unsigned char>> cubeMapImages;
+		std::unordered_map<CubeMapFace, std::vector<unsigned char>> cubeMapImages;
+		cubeMapImages.emplace(CubeMapFace::FRONT, std::vector<unsigned char>{});
+		cubeMapImages.emplace(CubeMapFace::RIGHT, std::vector<unsigned char>{});
+		cubeMapImages.emplace(CubeMapFace::BACK, std::vector<unsigned char>{});
+		cubeMapImages.emplace(CubeMapFace::LEFT, std::vector<unsigned char>{});
+		cubeMapImages.emplace(CubeMapFace::UPPER, std::vector<unsigned char>{});
+		cubeMapImages.emplace(CubeMapFace::LOWER, std::vector<unsigned char>{});
 
 		for (int componentIndex = 0; componentIndex < imageLength; componentIndex += actualComponents)
 		{
@@ -62,7 +70,7 @@ namespace Engine::Scenes
 
 			// Mapping UV coordinates onto a unit sphere and calculating spherical coordinates.
 			float azimuthDegrees = fmodf((360.0f * uvCoordinateX) + 180.0f, 360.0f);
-			float zenithDegrees = (180.0f * uvCoordinateY) - 90.0f;
+			float zenithDegrees = -((180.0f * uvCoordinateY) - 90.0f);
 
 			// Calculating cartesian (x, y, z) coordinates in world space from spherical coordinates. 
 			// Assuming that the origin is the center of the sphere, we are using a left-handed coordinate 
@@ -76,13 +84,13 @@ namespace Engine::Scenes
 			// Now we calculate the dot product with all axes and get the maximum (which will be the closest to 1 because
 			// both vectors are normalized). By doing this, we find which axis our vector representing cartesian coordinates on the sphere
 			// is most aligned with.
-			std::map<glm::vec3, float> dotProducts;
-			dotProducts.emplace(positiveXAxis, glm::dot(cartesianCoordinatesOnSphere, positiveXAxis));
-			dotProducts.emplace(positiveYAxis, glm::dot(cartesianCoordinatesOnSphere, positiveYAxis));
-			dotProducts.emplace(positiveZAxis, glm::dot(cartesianCoordinatesOnSphere, positiveZAxis));
-			dotProducts.emplace(negativeXAxis, glm::dot(cartesianCoordinatesOnSphere, negativeXAxis));
-			dotProducts.emplace(negativeYAxis, glm::dot(cartesianCoordinatesOnSphere, negativeYAxis));
-			dotProducts.emplace(negativeZAxis, glm::dot(cartesianCoordinatesOnSphere, negativeZAxis));
+			std::vector<std::pair<glm::vec3, float>> dotProducts;
+			dotProducts.push_back(std::make_pair(positiveXAxis, glm::dot(cartesianCoordinatesOnSphere, positiveXAxis)));
+			dotProducts.push_back(std::make_pair(positiveYAxis, glm::dot(cartesianCoordinatesOnSphere, positiveYAxis)));
+			dotProducts.push_back(std::make_pair(positiveZAxis, glm::dot(cartesianCoordinatesOnSphere, positiveZAxis)));
+			dotProducts.push_back(std::make_pair(negativeXAxis, glm::dot(cartesianCoordinatesOnSphere, negativeXAxis)));
+			dotProducts.push_back(std::make_pair(negativeYAxis, glm::dot(cartesianCoordinatesOnSphere, negativeYAxis)));
+			dotProducts.push_back(std::make_pair(negativeZAxis, glm::dot(cartesianCoordinatesOnSphere, negativeZAxis)));
 			glm::vec3 closestAxis = positiveXAxis;
 			float maxDotProduct = -1.0f;
 
@@ -105,6 +113,10 @@ namespace Engine::Scenes
 
 			// The pixel belongs to the cube map's face on the right.
 			if (closestAxis == positiveXAxis) {
+				cubeMapImages[CubeMapFace::RIGHT].push_back(image[componentIndex]);
+				cubeMapImages[CubeMapFace::RIGHT].push_back(image[componentIndex+1]);
+				cubeMapImages[CubeMapFace::RIGHT].push_back(image[componentIndex+2]);
+				cubeMapImages[CubeMapFace::RIGHT].push_back(image[componentIndex+3]);
 				localXAngle = acos(dot(glm::vec3(cartesianCoordinatesOnSphere.x, 0.0f, cartesianCoordinatesOnSphere.z), closestAxis));
 				localYAngle = acos(dot(glm::vec3(cartesianCoordinatesOnSphere.x, cartesianCoordinatesOnSphere.y, 0.0f), closestAxis));
 				localXAngle = cartesianCoordinatesOnSphere.z < 0 ? abs(localXAngle) : -1 * abs(localXAngle);
@@ -113,6 +125,10 @@ namespace Engine::Scenes
 
 			// The pixel belongs to the cube map's face above.
 			if (closestAxis == positiveYAxis) {
+				cubeMapImages[CubeMapFace::UPPER].push_back(image[componentIndex]);
+				cubeMapImages[CubeMapFace::UPPER].push_back(image[componentIndex + 1]);
+				cubeMapImages[CubeMapFace::UPPER].push_back(image[componentIndex + 2]);
+				cubeMapImages[CubeMapFace::UPPER].push_back(image[componentIndex + 3]);
 				localXAngle = acos(dot(glm::normalize(glm::vec3(cartesianCoordinatesOnSphere.x, cartesianCoordinatesOnSphere.y, 0.0f)), closestAxis));
 				localYAngle = acos(dot(glm::normalize(glm::vec3(0.0f, cartesianCoordinatesOnSphere.y, cartesianCoordinatesOnSphere.z)), closestAxis));
 				localXAngle = cartesianCoordinatesOnSphere.x > 0 ? abs(localXAngle) : -1 * abs(localXAngle);
@@ -121,6 +137,10 @@ namespace Engine::Scenes
 
 			// The pixel belongs to the cube map's face in front.
 			if (closestAxis == positiveZAxis) {
+				cubeMapImages[CubeMapFace::FRONT].push_back(image[componentIndex]);
+				cubeMapImages[CubeMapFace::FRONT].push_back(image[componentIndex + 1]);
+				cubeMapImages[CubeMapFace::FRONT].push_back(image[componentIndex + 2]);
+				cubeMapImages[CubeMapFace::FRONT].push_back(image[componentIndex + 3]);
 				localXAngle = acos(dot(glm::normalize(glm::vec3(cartesianCoordinatesOnSphere.x, 0.0f, cartesianCoordinatesOnSphere.z)), closestAxis));
 				localYAngle = acos(dot(glm::normalize(glm::vec3(0.0f, cartesianCoordinatesOnSphere.y, cartesianCoordinatesOnSphere.z)), closestAxis));
 				localXAngle = cartesianCoordinatesOnSphere.x > 0 ? abs(localXAngle) : -1 * abs(localXAngle);
@@ -129,6 +149,10 @@ namespace Engine::Scenes
 
 			// The pixel belongs to the cube map's face on the left.
 			if (closestAxis == negativeXAxis) {
+				cubeMapImages[CubeMapFace::LEFT].push_back(image[componentIndex]);
+				cubeMapImages[CubeMapFace::LEFT].push_back(image[componentIndex + 1]);
+				cubeMapImages[CubeMapFace::LEFT].push_back(image[componentIndex + 2]);
+				cubeMapImages[CubeMapFace::LEFT].push_back(image[componentIndex + 3]);
 				localXAngle = acos(dot(glm::normalize(glm::vec3(cartesianCoordinatesOnSphere.x, 0.0f, cartesianCoordinatesOnSphere.z)), closestAxis));
 				localYAngle = acos(dot(glm::normalize(glm::vec3(cartesianCoordinatesOnSphere.x, cartesianCoordinatesOnSphere.y, 0.0f)), closestAxis));
 				localXAngle = cartesianCoordinatesOnSphere.x > 0 ? abs(localXAngle) : -1 * abs(localXAngle);
@@ -137,6 +161,10 @@ namespace Engine::Scenes
 
 			// The pixel belongs to the cube map's face below.
 			if (closestAxis == negativeYAxis) {
+				cubeMapImages[CubeMapFace::LOWER].push_back(image[componentIndex]);
+				cubeMapImages[CubeMapFace::LOWER].push_back(image[componentIndex + 1]);
+				cubeMapImages[CubeMapFace::LOWER].push_back(image[componentIndex + 2]);
+				cubeMapImages[CubeMapFace::LOWER].push_back(image[componentIndex + 3]);
 				localXAngle = acos(dot(glm::normalize(glm::vec3(cartesianCoordinatesOnSphere.x, cartesianCoordinatesOnSphere.y, 0.0f)), closestAxis));
 				localYAngle = acos(dot(glm::normalize(glm::vec3(cartesianCoordinatesOnSphere.x, cartesianCoordinatesOnSphere.y, 0.0f)), closestAxis));
 				localXAngle = cartesianCoordinatesOnSphere.x > 0 ? abs(localXAngle) : -1 * abs(localXAngle);
@@ -145,6 +173,10 @@ namespace Engine::Scenes
 
 			// The pixel belongs to the cube map's face behind.
 			if (closestAxis == negativeZAxis) {
+				cubeMapImages[CubeMapFace::BACK].push_back(image[componentIndex]);
+				cubeMapImages[CubeMapFace::BACK].push_back(image[componentIndex + 1]);
+				cubeMapImages[CubeMapFace::BACK].push_back(image[componentIndex + 2]);
+				cubeMapImages[CubeMapFace::BACK].push_back(image[componentIndex + 3]);
 				localXAngle = acos(dot(glm::normalize(glm::vec3(cartesianCoordinatesOnSphere.x, cartesianCoordinatesOnSphere.y, 0.0f)), closestAxis));
 				localYAngle = acos(dot(glm::normalize(glm::vec3(0.0f, cartesianCoordinatesOnSphere.y, cartesianCoordinatesOnSphere.z)), closestAxis));
 				localXAngle = cartesianCoordinatesOnSphere.x < 0 ? abs(localXAngle) : -1 * abs(localXAngle);
@@ -153,16 +185,17 @@ namespace Engine::Scenes
 
 			// This calculates the UV coordinates relative to the cube map's face we detected that the pixel belongs to.
 			UVCoordinateX = (tan(localXAngle) + 1.0f) * 0.5f;
-			UVCoordinateY = -(tan(localYAngle) + 1.0f) * 0.5f;
+			UVCoordinateY = (tan(localYAngle) + 1.0f) * 0.5f;
 
-
+			// We have 4 side faces on the cube map, so we divide the width of the spherical HDRi image by 4.
+			// That will give us the width in pixels each individual image of the cube map needs to be.
 
 			// Clumping the color and the coordinate of that pixel as if the image was folded on itself into a sphere.
-			unsigned int color = 0;
+			/*unsigned int color = 0;
 
 			for (int c = actualComponents; c > 0; --c) {
 				color |= image[componentIndex + c] << (8 * (c - 1));
-			}
+			}*/
 
 			/*unsigned int r = image[componentIndex];
 			unsigned int g = image[componentIndex+1];
@@ -170,6 +203,14 @@ namespace Engine::Scenes
 
 			/*_pixelColors[pixelIndex] = glm::vec3((float)image[componentIndex], (float)image[componentIndex + 1], (float)image[componentIndex + 2]);
 			_pixelCoordinatesWorldSpace[pixelIndex] = glm::vec3(sphereCoordinateX, sphereCoordinateY, sphereCoordinateZ);*/
+		}
+
+
+
+		int c = 0;
+		for (auto i = cubeMapImages.begin(); i != cubeMapImages.end(); ++i) {
+			stbi_write_png(std::to_string(c).c_str(), _width / 4, _width / 4, 4, &i->second, _width);
+			++c;
 		}
 
 		std::cout << "Environment map " << imageFilePath.string() << " loaded." << std::endl;
