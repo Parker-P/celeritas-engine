23ea788f7d061dc1b3074a78d891d06da016f773
DESKTOP-R0D6P0C\Paolo <parker9694@gmail.com>
2024-03-30 22:34:05 +0100

    Fixed the directions scene.

--- src/engine/vulkan/VulkanApplication.cpp
+++ src/engine/vulkan/VulkanApplication.cpp
@@ -372,10 +372,10 @@ namespace Engine::Vulkan
 	{
 		//auto scenePath = Settings::Paths::ModelsPath() /= "MaterialSphere.glb";
 		//auto scenePath = Settings::Paths::ModelsPath() /= "cubes.glb";
-		//auto scenePath = Settings::Paths::ModelsPath() /= "directions.glb";
+		auto scenePath = Settings::Paths::ModelsPath() /= "directions.glb";
 		//auto scenePath = Settings::Paths::ModelsPath() /= "f.glb";
 		//auto scenePath = Settings::Paths::ModelsPath() /= "fr.glb";
-		auto scenePath = Settings::Paths::ModelsPath() /= "mp5k.glb";
+		//auto scenePath = Settings::Paths::ModelsPath() /= "mp5k.glb";
 		//auto scenePath = Settings::Paths::ModelsPath() /= "Cube.glb";
 		//auto scenePath = Settings::Paths::ModelsPath() /= "stanford_dragon_pbr.glb";
 		//auto scenePath = Settings::Paths::ModelsPath() /= "SampleMap.glb";
